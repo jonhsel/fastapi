5b769c3a61af271bf7db77e5fe0594bc0f941333
jonhsel <jonhselmo.engcomp@gmail.com>
2025-03-18 13:53:20 -0300

    put metodo

--- secao03/main.py
+++ secao03/main.py
@@ -38,16 +38,28 @@ async def get_curso(curso_id: int):
             status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado!"
         )
 
-@app.post('/cursos')
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
 #async def post_curso(curso: Optional[Curso]=None):
 async def post_curso(curso: Curso):
-    if curso.id not in cursos:
+    '''if curso.id not in cursos:
         cursos[curso.id] = curso
         return curso
     else:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Já existe um curso com ID {curso.id}")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, 
+                            detail=f"Já existe um curso com ID {curso.id}")'''
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id
+    return  curso
     
-
+@app.put('/cursos')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Não existe um curso com esse {curso_id}.")
 
 
 if __name__ == '__main__':
