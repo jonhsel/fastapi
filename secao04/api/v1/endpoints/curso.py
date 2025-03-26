352b5ed7c62e946463577a2d84a8fd87c2436f3f
jonhsel <jonhselmo.engcomp@gmail.com>
2025-03-26 05:57:27 -0300

    secao 04 api

---
+++ secao04/api/v1/endpoints/curso.py
@@ -0,0 +1,39 @@
+from typing import List
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import Response
+from fastapi import status
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.curso_model import CursoModel
+from schema.curso_schema import CursoSchema
+from core.deps import get_session
+
+router = APIRouter()
+
+
+#POST curso
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=CursoSchema)
+async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    novo_curso = CursoModel(titulo=curso.titulo, 
+                            aulas=curso.aulas,
+                            horas=curso.horas)
+    db.add(novo_curso)
+    await db.commit()
+
+    return novo_curso
+
+@router.get_cursos('/', response_model=List[CursoSchema])
+async def get_cursos(db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel)
+        result = await session.execute(query)
+        cursos: List[CursoModel] = result.scalars().all()
+
+        return cursos
+    
+    
+    
--- secao04/core/configs.py
+++ secao04/core/configs.py
@@ -1,5 +1,5 @@
 from pydantic import BaseSettings
-from sqlalchemy.ext.declarative import delcarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class Settings(BaseSettings):
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
         Configurações gerais usadas na aplicação
     """
     API_V1_STR: str = "api/v1"
-    DB__URL: str = "postgresql+asyncpg://geek:university@localhost:5432/faculdade"
-    DBBaseModel = delcarative_base()
+    DB__URL: str = "postgresql+asyncpg://jonhnoah:furia333@localhost:5432/faculdade"
+    DBBaseModel = declarative_base()
 
     class Config:
         case_sensitive = True
